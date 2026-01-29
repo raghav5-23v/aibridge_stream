92850f870bab819e15c7cf86467111b1e0a94f2f
Raghav <raghav@23v.co>
2026-01-29 19:19:17 +0530

    added redis credentials

--- app.py
+++ app.py
@@ -18,10 +18,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-REDIS_HOST = "18.214.160.94"
-REDIS_PORT = 6379
-REDIS_DB = 0
-REDIS_PASSWORD = "redis"
+REDIS_HOST = os.getenv("CACHE_REDIS_HOST", "")
+REDIS_PORT = int(os.getenv("CACHE_REDIS_PORT", 6379))
+REDIS_DB = int(os.getenv("CACHE_REDIS_DB", 0))
+REDIS_PASSWORD = os.getenv("CACHE_REDIS_PASSWORD", "redis")
+
+# REDIS_HOST = "18.214.160.94"
+# REDIS_PORT = 6379
+# REDIS_DB = 0
+# REDIS_PASSWORD = "redis"
 
 redis_client = Redis(
     host=REDIS_HOST,
